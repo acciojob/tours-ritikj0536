122a16b841b21ee018bcd1c96f033b703cded8aa
ritikj0536 <jharitik64@gmail.com>
2025-12-24 15:38:59 +0530

    tours

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -2,22 +2,29 @@ import React, { useState } from "react";
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
-  return (
-    <div>
-      <article>
-        <img src={image} alt={name} />
-        <h3>{name}</h3>
-        <h4>${price}</h4>
 
-        <p>
-          {readMore ? info : `${info.substring(onabort, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? "See less" : "Show more"}
-          </button>
-          <button className="remove-btn" onClick={() => removeTour(id)}>Remove</button>
-        </p>
-      </article>
-    </div>
+  return (
+    <article id={`tour-item-${id}`}>
+      <img src={image} alt={name} />
+
+      <h3>{name}</h3>
+      <h4>${price}</h4>
+
+      <p id={`tour-item-para-${id}`}>
+        {readMore ? info : `${info.substring(0, 200)}`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "See less" : "Show more"}
+        </button>
+      </p>
+
+      <button
+        id={`delete-btn-${id}`}
+        className="remove-btn"
+        onClick={() => removeTour(id)}
+      >
+        Remove
+      </button>
+    </article>
   );
 };
 
